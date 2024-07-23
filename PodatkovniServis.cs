51b163f809cfb0a93eefd3764618a502b8fe1f54
Josip Borozan <joske0000@gmail.com>
2024-07-23 21:01:37 +0200

    22

---
+++ PodatkovniServis.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToSql
+{
+    internal class PodatkovniServis
+    {
+        private readonly LinqSqlDataContext _dataContext;
+
+        public PodatkovniServis(LinqSqlDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public void UnosNovogZaposlenika(Zaposlenik zaposlenik)
+        {
+            _dataContext.Zaposleniks.InsertOnSubmit(zaposlenik);
+            _dataContext.SubmitChanges();
+        }
+
+        public void UnosNovogZaposlenika(string imePrezime, string email, string telefon, string adresa, int odjelId)
+        {
+            var zaposlenik = new Zaposlenik
+            {
+                ImePrezime = imePrezime,
+                Email = email,
+                Telefon = telefon,
+                Adresa = adresa,
+                OdjelID = odjelId
+            };
+
+            _dataContext.Zaposleniks.InsertOnSubmit(zaposlenik);
+            _dataContext.SubmitChanges();
+        }
+
+        public void AzuriranjeZaposlenika(Zaposlenik zaposlenik)
+        {
+            _dataContext.SubmitChanges();
+        }
+
+        public void AzuriranjeZaposlenika(int id, string telefon, string adresa)
+        {
+            var zaposlenik = DohvatiZaposlenika(id);
+            if (zaposlenik != null)
+            {
+                zaposlenik.Telefon = telefon;
+                zaposlenik.Adresa = adresa;
+
+                _dataContext.SubmitChanges();
+            }
+        }
+
+        public Zaposlenik DohvatiZaposlenika(int id)
+        {
+            return _dataContext.Zaposleniks.SingleOrDefault(_ => _.ID == id);
+        }
+
+        public Zaposlenik DohvatiZaposlenika(string imePrezime)
+        {
+            return _dataContext.Zaposleniks.Where(_ => _.ImePrezime.Contains(imePrezime))
+                .FirstOrDefault();
+        }
+
+        public void ObrisiZaposlenika(int id)
+        {
+            var zaposlenik = DohvatiZaposlenika(id);
+
+            _dataContext.Zaposleniks.DeleteOnSubmit(zaposlenik);
+            _dataContext.SubmitChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
+using System.Configuration;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace LinqToSql
 {
@@ -11,38 +11,62 @@ namespace LinqToSql
     {
         static void Main(string[] args)
         {
+            var connStr = ConfigurationManager.ConnectionStrings["DbCon"].ConnectionString;
+            var dbContext = new LinqSqlDataContext(connStr);
 
-            var dbContext = new LinqSqlDataContext("Data Source=JOSE\\MSSQLSERVER01;Initial Catalog=linq_to_sql;Integrated Security=True;Encrypt=False;TrustServerCertificate=True");
+            // var dbContext = new LinqSqlDataContext("Data Source=JOSE\\MSSQLSERVER01;Initial Catalog=linq_to_sql;Integrated Security=True;Encrypt=False;TrustServerCertificate=True");
 
             // UnosZaposlenika(dbContext);
+            // var dbContext = new LinqToSqlDataContext("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=linq_to_sql;Integrated Security=True");
 
-            var upit = from zaposlenik in dbContext.Zaposleniks
-                       where zaposlenik.ImePrezime.StartsWith("Ivan")
-                       select zaposlenik
-                       ;
+            // DML 
 
-            
+            UnosZaposlenika(dbContext);
 
+            var servis = new PodatkovniServis(dbContext);
+
+            servis.UnosNovogZaposlenika(new Zaposlenik
+            {
+                ImePrezime = "Iva Ivić",
+                Email = "iva.ivic@algebra.hr",
+                Telefon = "095444333222",
+                Adresa = "Livadska 12",
+                OdjelID = 2
+            });
+
+            // DQL 
+
+            var upit = dbContext.Zaposleniks.FirstOrDefault();
+
+            //var upit = from zaposlenik in dbContext.Zaposleniks
+            //           where zaposlenik.ImePrezime.StartsWith("Pero")
+            //           select zaposlenik;
+
+            var zaposlenik = servis.DohvatiZaposlenika("Pero");
+            zaposlenik.OdjelID = 3;
+
+            servis.AzuriranjeZaposlenika(zaposlenik);
+
+            servis.AzuriranjeZaposlenika(2, "+385 42 111 222", "Kolodvorska 145");
+
+            servis.ObrisiZaposlenika(2);
 
             Console.ReadKey();
         }
 
-
-
-
         private static void UnosZaposlenika(LinqSqlDataContext dbContext)
         {
-            var zaposlenik = new Zaposlenik()
+            var zaposlenik = new Zaposlenik
             {
-                ImePrezime = "Ivan Horvat",
-                Email = "ivanhorvat@gmail.com",
-                Telefon = "0981234567",
-                Adresa = "Zagreb",
+                ImePrezime = "Pero Perić",
+                Email = "pero.peric@algebra.hr",
+                Telefon = "09811122233",
+                Adresa = "Vrtna bb",
                 OdjelID = 1
             };
 
-            dbContext.Zaposleniks.InsertOnSubmit(zaposlenik);    
-            
+            dbContext.Zaposleniks.InsertOnSubmit(zaposlenik);
+
             dbContext.SubmitChanges();
         }
     }
